42696313df1ec25effe9cd4b198621bc819c4d4c
Adrien FORT <adrien.fort01@gmail.com>
2021-12-14 01:12:40 +0100

    feat: add step 5

---
+++ pages/Posts.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import useSWR from "swr";
+import axios from "axios";
+
+import styles from "../styles/Posts.module.css";
+
+const fetcher = (url) => axios.get(url).then((res) => res.data);
+
+const useMounted = () => {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+  return mounted;
+};
+
+function Posts() {
+  const mounted = useMounted();
+  const { data, error } = useSWR(
+    () => (mounted ? "/api/posts" : null),
+    fetcher
+  );
+
+  if (error) {
+    return <div>Error</div>;
+  }
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  if (data) {
+    return (
+      <div className={styles.postsContainer}>
+        {data.posts.map((post, i) => (
+          <div key={i} className={styles.postsElem}>
+            <br />
+            {post.title}
+            <br />
+            <br />
+            {post.content}
+          </div>
+        ))}
+      </div>
+    );
+  }
+  return <div>Error in load</div>;
+}
+
+export default Posts;
