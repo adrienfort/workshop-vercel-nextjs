785a58318d23a0c3fb542c595cb15ba01c08f325
Adrien FORT <adrien.fort01@gmail.com>
2021-12-14 01:08:12 +0100

    feat: add step 4

---
+++ pages/TopBar.js
@@ -0,0 +1,11 @@
+import styles from "../styles/TopBar.module.css";
+
+function TopBar() {
+  return (
+    <div>
+      <h2 className={styles.topBar}>My cool blog</h2>
+    </div>
+  );
+}
+
+export default TopBar;
